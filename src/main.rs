7f6dcd41d87ec3e4b17de34a117d72e069590a73
moisutsu <moisutsu@gmail.com>
2020-09-22 23:27:18 +0900

    Add implementation to display the contents of .ipynb file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Clap;
+use nbcat::{Cell, Ipynb, Opts};
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+
+    let file = std::fs::read_to_string(&opts.input_file)?;
+    let ipynb: Ipynb = serde_json::from_str(&file)?;
+    for cell in ipynb.cells {
+        display_source(&cell);
+        println!("");
+        display_output(&cell);
+        println!("");
+    }
+    Ok(())
+}
+
+fn display_source(cell: &Cell) {
+    println!(
+        "[{}]:",
+        if let Some(execution_count) = cell.execution_count {
+            execution_count.to_string()
+        } else {
+            " ".to_string()
+        }
+    );
+    println!("{}", cell.source.join(""));
+}
+
+fn display_output(cell: &Cell) {
+    for output in cell.outputs.iter() {
+        if let Some(text) = &output.text {
+            println!("{}", text.join(""));
+        } else if let Some(data) = &output.data {
+            if let Some(text_plain) = &data.text_plain {
+                println!("{}", text_plain.join(""));
+            }
+        }
+    }
 }
