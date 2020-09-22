4176c432615b29d32bbe764adb7350724893d295
moisutsu <moisutsu@gmail.com>
2020-09-22 11:48:26 +0900

    Add a model for storing .ipynb files

---
+++ src/model.rs
@@ -0,0 +1,27 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Ipynb {
+    pub cells: Vec<Cell>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Cell {
+    pub cell_type: String,
+    pub execution_count: Option<i32>,
+    pub outputs: Vec<Output>,
+    pub source: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Output {
+    pub output_type: String,
+    pub data: Option<Data>,
+    pub text: Option<Vec<String>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Data {
+    #[serde(rename(serialize = "text/plain"))]
+    pub text_plain: Option<Vec<String>>,
+}
