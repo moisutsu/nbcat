8e5190cc1c0408b13b3ea23dc249db3827c879fd
moisutsu <moisutsu@gmail.com>
2021-06-21 14:52:01 +0900

    Use anyhow::Result instead of unwrap()

--- src/display.rs
+++ src/display.rs
@@ -1,18 +1,21 @@
+use anyhow::Result;
 use image::DynamicImage;
 
 use crate::{Cell, Ipynb};
 
-pub fn display_ipynb(ipynb: &Ipynb) {
+pub fn display_ipynb(ipynb: &Ipynb) -> Result<()> {
     for cell in &ipynb.cells {
-        display_cell(cell);
+        display_cell(cell)?;
     }
+    Ok(())
 }
 
-fn display_cell(cell: &Cell) {
+fn display_cell(cell: &Cell) -> Result<()> {
     display_source(&cell);
     println!();
-    display_output(&cell);
+    display_output(&cell)?;
     println!();
+    Ok(())
 }
 
 fn display_source(cell: &Cell) {
@@ -27,7 +30,7 @@ fn display_source(cell: &Cell) {
     println!("{}", cell.source.join(""));
 }
 
-fn display_output(cell: &Cell) {
+fn display_output(cell: &Cell) -> Result<()> {
     println!(
         "Out: [{}]",
         if let Some(execution_count) = cell.execution_count {
@@ -41,16 +44,17 @@ fn display_output(cell: &Cell) {
             println!("{}", text.join(""));
         } else if let Some(data) = &output.data {
             if let Some(image_png) = &data.image_png {
-                display_image_png(image_png);
+                display_image_png(image_png)?;
             } else if let Some(text_plain) = &data.text_plain {
                 println!("{}", text_plain.join(""));
             }
         }
     }
+    Ok(())
 }
 
-fn display_image_png(image_png: &str) {
-    let img = image::load_from_memory(&base64::decode(image_png.trim_end()).unwrap()[..]).unwrap();
+fn display_image_png(image_png: &str) -> Result<()> {
+    let img = image::load_from_memory(&base64::decode(image_png.trim_end())?[..])?;
 
     let display_config = viuer::Config {
         transparent: true,
@@ -58,5 +62,6 @@ fn display_image_png(image_png: &str) {
         ..Default::default()
     };
 
-    viuer::print(&DynamicImage::ImageRgb8(img.to_rgb8()), &display_config).unwrap();
+    viuer::print(&DynamicImage::ImageRgb8(img.to_rgb8()), &display_config)?;
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+use anyhow::Result;
 use clap::Clap;
 use nbcat::{display_ipynb, Ipynb, Opts};
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<()> {
     let opts = Opts::parse();
 
     let file = std::fs::read_to_string(&opts.input)?;
     let ipynb: Ipynb = serde_json::from_str(&file)?;
-    display_ipynb(&ipynb);
+    display_ipynb(&ipynb)?;
     Ok(())
 }
