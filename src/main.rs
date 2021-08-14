0aac4fc24df8ab2f46f68082311116ebe51a0730
moisutsu <moisutsu@gmail.com>
2021-08-14 15:02:29 +0900

    Remove unnecessary type annotations

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use anyhow::Result;
 use clap::Clap;
-use nbcat::{display_ipynb, Ipynb, Opts};
+use nbcat::{display_ipynb, Opts};
 
 fn main() -> Result<()> {
     let opts = Opts::parse();
 
     let file = std::fs::read_to_string(&opts.input)?;
-    let ipynb: Ipynb = serde_json::from_str(&file)?;
+    let ipynb = serde_json::from_str(&file)?;
     display_ipynb(&ipynb)?;
     Ok(())
 }
